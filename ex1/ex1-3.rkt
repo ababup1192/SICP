92f830eb0ef73d2340e172df341c1dd58f1a64ea
ababup1192 <ababup1192@gmail.com>
2015-12-16 23:31:23 +0900

    first commit

---
+++ ex1/ex1-3.rkt
@@ -0,0 +1,18 @@
+#lang racket
+(define (square x) (* x x))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y))
+)
+
+(define (bigger-sum-of-squares x y z)
+  (cond ((> x y z) (sum-of-squares x y))
+        ((< x y z) (sum-of-squares y z))
+        (else (sum-of-squares x z))
+  )
+)
+
+(bigger-sum-of-squares 1 2 3)
+(bigger-sum-of-squares 3 2 1)
+(bigger-sum-of-squares 3 1 2)
+
